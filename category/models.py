c63fb7015a644343aa7cb207bfc14c19f1c62856
joe342wise <jnyarko342@gmail.com>
2024-07-01 00:22:43 +0000

    feat: Add CategoryTable model to category app

--- category/models.py
+++ category/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class CategoryTable(models.Model):
+    catigoryId = models.AutoField(primary_key=True, db_column='catID')
+    catigoryName = models.CharField(max_length=50, null=False, db_column='catName')
+
+    def __str__(self):
+        return self.catigoryName
+    
+    class Meta:
+        app_label = 'category'
+        db_table = 'tblCategory'
+        # managed = True
+        # verbose_name = 'Category'
+        # verbose_name_plural = 'Categories'
+        # ordering = ['catigoryName']
