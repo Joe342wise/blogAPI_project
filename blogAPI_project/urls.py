69220e9b6140021284389e3130c84bf8bb0a491d
joe342wise <jnyarko342@gmail.com>
2024-07-01 00:15:43 +0000

    feat: Add user URLs and include them in the main project URLs

--- blogAPI_project/urls.py
+++ blogAPI_project/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from user.urls import urlpatterns as user_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
