53ff5c1935f3798748ba5f896573ba7533f0f052
joe342wise <jnyarko342@gmail.com>
2024-07-02 11:31:08 +0000

    Refactor postTag models to use foreign keys for postId and tagId

--- postTag/models.py
+++ postTag/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class PostTagTable(models.Model):
+    postId = models.ForeignKey('posts.PostTable', on_delete=models.CASCADE, db_column='postID')
+    tagId = models.ForeignKey('tag.TagTable', on_delete=models.CASCADE, db_column='tagID')
+
+    class Meta:
+        app_lable = 'postTag'
+        db_table = 'tblPostTag'
