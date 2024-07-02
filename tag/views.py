88fd2697871418be2021d460ac0cad2bd9263cff
joe342wise <jnyarko342@gmail.com>
2024-07-02 12:16:10 +0000

    Refactor TagTable model to use AutoField for tagId and add tableName meta option

--- tag/views.py
+++ tag/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from tag.models import TagTable as Tag
+from tag.serializers import TagSerializer
 
 # Create your views here.
+class TagViewSet(viewsets.ModelViewSet):
+    queryset= Tag.objects.all()
+    serializer_class= TagSerializer
