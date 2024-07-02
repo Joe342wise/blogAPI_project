c36341e1fc0208a5138afe074a46acedbe8478e8
joe342wise <jnyarko342@gmail.com>
2024-07-02 11:20:32 +0000

    feat: Add CommentViewSet for CommentTable model

--- comment/views.py
+++ comment/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from comment.models import CommentTable as Comment
+from comment.serializers import CommentSerializer
 
 # Create your views here.
+class CommentViewSet(viewsets.ModelViewSet):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
