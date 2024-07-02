257145488cda14ae84d6436e2793f873b65bb2cc
joe342wise <jnyarko342@gmail.com>
2024-07-02 12:52:25 +0000

    Refactor app names in settings.py to match model names

--- postTag/urls.py
+++ postTag/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from postTag.views import PostTagViewSet
 
-router = DefaultRouter
+router = DefaultRouter()
 router.register(r'postTag', PostTagViewSet)
 
 urlPatterns = [
--- tag/urls.py
+++ tag/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from tag.views import TagViewSet
 
-router = DefaultRouter
+router = DefaultRouter()
 router.register(r'tag', TagViewSet)
 
 urlPatterns = [
