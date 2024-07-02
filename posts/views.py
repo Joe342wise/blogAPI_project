36b4e10b7755636b3b8bde6e33ad63830929a69c
joe342wise <jnyarko342@gmail.com>
2024-07-02 11:02:52 +0000

    feat: Add PostViewSet for PostTable model

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from posts.models import PostTable as Post
+from posts.serializers import PostSerializer
 
 # Create your views here.
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
