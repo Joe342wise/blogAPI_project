3a1195cf8fe998fff7f600892d38362a55f6828a
joe342wise <jnyarko342@gmail.com>
2024-07-02 12:15:18 +0000

    Refactor TagTable model to use AutoField for tagId and add tableName meta option

--- tag/models.py
+++ tag/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class TagTable(models.Model):
+    tagId = models.AutoField(primary_key=True, db_column='tagID')
+    tagName = models.CharField(max_length=50, null=False, db_column='tagName')
+
+    def __str__(self):
+        return self.tagName
+    
+    class Meta:
+        app_lable = 'tag'
+        db_table = 'tblTag'
