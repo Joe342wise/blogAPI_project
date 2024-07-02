bc66f4f9ac43caeaabcfd3dc3388f65a79b31716
joe342wise <jnyarko342@gmail.com>
2024-07-02 11:47:53 +0000

    feat: Add PostTagView for PostTagTable model

--- postTag/views.py
+++ postTag/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from postTag.models import PostTagTable as PostTag
+from postTag.serializers import PostTagSerializer
 
 # Create your views here.
+class PostTagView(viewsets.ModelViewSet):
+    queryset = PostTag.objects.all()
+    serializer_class = PostTagSerializer
