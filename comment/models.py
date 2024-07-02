d29a75d066c24ab9c12967c5cc8bfedd250f8525
joe342wise <jnyarko342@gmail.com>
2024-07-02 11:12:27 +0000

    Add CommentTable model to comment app

--- comment/models.py
+++ comment/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class CommentTable(models.Model):
+    commentId = models.AutoField(primary_key=True, db_column='commentID')
+    postId = models.ForeignKey('posts.PostTable', on_delete=models.CASCADE, db_column='postID')
+    userId = models.ForeignKey('user.UserTable', on_delete=models.CASCADE, db_column='userID')
+    content = models.TextField(db_column='commentContent')
+    createdTime = models.DateTimeField(db_column='commentCreatedAt')
+    
+    def __str__(self):
+        return self.content
+    
+    class Meta:
+        app_label = 'comment'
+        db_table = 'tblComment'
